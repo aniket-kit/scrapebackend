a4be466f89d752103c14662d721e5df9246b5229
Aniket kit <121393155+aniket-kit@users.noreply.github.com>
2025-05-25 15:50:19 +0530

    Add files via upload

--- backend/routes/scrapeRoutes.js
+++ backend/routes/scrapeRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { scrapeNow, getData } = require('../controllers/scrapeController');
 
-router.post('/api/scrape', scrapeNow); // 👈 changed from GET to POST
-router.get('/api/data', getData);
+router.post('/scrape', scrapeNow); // 👈 changed from GET to POST
+router.get('/data', getData);
 
 module.exports = router;
