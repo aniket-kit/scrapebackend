bfc736c7e30df1545e305d73c7958b0402e5c643
Aniket kit <121393155+aniket-kit@users.noreply.github.com>
2025-05-25 14:53:38 +0530

    Delete routes directory

--- routes/scrapeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { scrapeNow, getData } = require('../controllers/scrapeController');
-
-router.post('/api/scrape', scrapeNow); // 👈 changed from GET to POST
-router.get('/api/data', getData);
-
-module.exports = router;
